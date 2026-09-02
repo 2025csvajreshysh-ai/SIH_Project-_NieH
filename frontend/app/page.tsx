48e837bc486dafac2046327398f85648e4fe5774
git config --global user.name Swordsman <vinaykmpro2007@gmail.com>
2026-09-02 21:45:08 +0530

    odified some files on frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
+import Forecast from "./Forecast";
 export default function Home() {
   return (
     <main>
       <h1>SIH Project</h1>
       <p>Welcome to our project.</p>
+      <Forecast />
     </main>
   );
 }
